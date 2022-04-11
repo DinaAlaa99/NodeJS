64189a2eee834f7b0250680cbce131cbe332a610
DinaAlaa99 <37483521+DinaAlaa99@users.noreply.github.com>
2022-04-11 04:54:15 +0200

    fix minor bug

--- Day1/ageCalculator.js
+++ Day1/ageCalculator.js
@@ -3,8 +3,8 @@ var calcAge = function (name,date) {
     var today = new Date()
     var birthDate = new Date(date)
    
-    if (birthDate.getFullYear() == 2020)
-        return "error"
+    if (birthDate.getFullYear() >= 2020)
+        return "you are too young"
     else
     { 
         var age = today.getFullYear() - birthDate.getFullYear()
--- Day1/app.js
+++ Day1/app.js
@@ -5,5 +5,5 @@ console.log(calculator.add(1, 2)) //3
 console.log(calculator.sub('a', 'b')) //error
 console.log(calculator.multiply(2, 5)) //10
 
-console.log(ageCalculator("Dina", "2020-03-02")) //error
+console.log(ageCalculator("Dina", "2020-03-02")) //you are too young
 console.log(ageCalculator("Dina", "1999-03-13")) //Hello Dina your age is 23
